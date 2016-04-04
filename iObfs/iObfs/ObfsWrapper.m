94d1aad49602356c04e5b2bf3fa2b1aee95fcbc4
Mike Tigas <mike@tig.as>
2016-04-03 22:14:51 -0400

    enable all supported PTs

--- iObfs/iObfs/ObfsWrapper.m
+++ iObfs/iObfs/ObfsWrapper.m
@@ -12,15 +12,21 @@
 @implementation ObfsWrapper
 
 @synthesize
-    obfsSocksPort = _obfsSocksPort,
+    obfs4SocksPort = _obfs4SocksPort,
     meekSocksPort = _meekSocksPort
+    obfs2SocksPort = _obfs2SocksPort,
+    obfs3SocksPort = _obfs3SocksPort,
+    scramblesuitSocksPort = _scramblesuitSocksPort,
 ;
 
 -(void)main {
     /* TODO: We should re-enable the port randomization and find a way to
      *       communicate it back to the main app thread. */
-    _obfsSocksPort = 47351;
+    _obfs4SocksPort = 47351;
     _meekSocksPort = 47352;
+    _obfs2SocksPort = 47353;
+    _obfs3SocksPort = 47354;
+    _scramblesuitSocksPort = 47355;
 
     GoIobfs4proxyMain();
 }
