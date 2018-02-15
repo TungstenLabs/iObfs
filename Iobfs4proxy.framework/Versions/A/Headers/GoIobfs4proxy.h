4ef77ca38cb19075f7bdc996e664511936c68c1b
Aleksander Zubala <alek@zubala.com>
2018-02-15 15:43:49 +0100

    Adds option to close obfs4 proxy so it can be restarted
    - fixed go compilation issues in build.sh
    - changed fork of obfs4 to YND with fixes that enables obfs4 close
    - bumped pod version to 1.0.2
    - updated bundled framework and zip URL for the pod distribution

--- Iobfs4proxy.framework/Versions/A/Headers/GoIobfs4proxy.h
+++ Iobfs4proxy.framework/Versions/A/Headers/GoIobfs4proxy.h
@@ -10,6 +10,8 @@
 #include "GoUniverse.h"
 
 
+FOUNDATION_EXPORT void GoIobfs4proxyClose();
+
 FOUNDATION_EXPORT void GoIobfs4proxyMain();
 
 #endif
--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cd "`dirname \"$0\"`"
 export REPOROOT=$(pwd)
 export GOPATH=$REPOROOT/.build
 export PATH=$GOPATH/bin:$PATH
+export CGO_CFLAGS_ALLOW='-fmodules|-fblocks'
 
 set -v
 
@@ -12,7 +13,7 @@ rm -fr $GOPATH
 mkdir -p $GOPATH
 
 # clean up previous build
-rm -fr Iobfs4proxy.framework/
+rm -fr Iobfs4proxy.framework
 
 # Set up gomobile.
 # Unfortunately, "go bind" currently skips compiling for i386
@@ -24,17 +25,17 @@ git fetch mtigas
 git checkout iobfs-build
 cd $REPOROOT
 go get -f -u golang.org/x/mobile/cmd/gomobile
-gomobile init
+eval $GOPATH/bin/gomobile init
 
 # get our forked obfs4proxy source and build it as a framework
 go get -d git.torproject.org/pluggable-transports/obfs4.git
 cd $GOPATH/src/git.torproject.org/pluggable-transports/obfs4.git
-git remote add mtigas https://github.com/mtigas/obfs4.git
-git fetch mtigas
-git checkout iObfs-201604-dev
+git remote add ynd https://github.com/ynd-consult-ug/obfs4.git
+git fetch ynd
+git checkout iObfs-201802
 cd $REPOROOT
 go get -f -u git.torproject.org/pluggable-transports/obfs4.git/obfs4proxy
-gomobile bind -target ios -v git.torproject.org/pluggable-transports/obfs4.git/obfs4proxy
+eval $GOPATH/bin/gomobile bind -target ios -v git.torproject.org/pluggable-transports/obfs4.git/obfs4proxy
 	
-# clean up build, everything we need is in Iobfs4proxy.framework/
-rm -fr $GOPATH
+#clean up build, everything we need is in Iobfs4proxy.framework/
+# rm -fr $GOPATH
